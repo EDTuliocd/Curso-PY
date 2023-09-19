2fe6b35b2c6990e15513b7dc6e1e6ba107885c89
TULIO CD <138325577+EDTuliocd@users.noreply.github.com>
2023-09-19 11:42:37 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,24 @@
+# Este es un comentario: Hola Mundo
+# Nuestro hola Mundo
+
+print("Hola Python")
+
+"""
+Estes es un 
+comentario
+doble
+"""
+"""
+ESte es un 
+comentario 
+en varias lineas
+"""
+print("Hola Mundo!")
+print("Hola Navegante")
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo  "str"
+print(type(5)) # Tipo "in"
+print(type(1.5)) # Tipo "Float"
+print(type(3 + 1j)) # Tipo "complex"
+print(type(True)) # Tipo "bool"
+
